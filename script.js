bd79fe5f4784db59992fb006202a704682f46b73
zawadahmed33 <zawadahmed03@gmail.com>
2024-07-08 13:43:59 +0600

    added functionality to store coeffecients

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 const degree = document.querySelector('.degree')
+let degreeCopy = 0;
 const submitBtn = document.querySelector('.submitBtn')
 
 const coeffs = document.querySelector('.coeffs')
 
+
+
 const appendInputs = (num) => {
   coeffs.innerHTML = ""
+  const coeffsForm = document.createElement("form")
   for (let i=0; i < num; i++) {
     const inputBox = document.createElement('input')
+    inputBox.type = "number"
+    inputBox.value = 0 
     inputBox.className = `${i}`
-    coeffs.appendChild(inputBox)
+    coeffsForm.appendChild(inputBox)
   }
   const coeffSubmit = document.createElement('button')
+  coeffSubmit.type = "submit"
   coeffSubmit.className = 'coeffSubmit'
   coeffSubmit.innerHTML = 'Submit'
-  coeffs.appendChild(coeffSubmit)
+  coeffsForm.appendChild(coeffSubmit)
+  coeffs.appendChild(coeffsForm)
+
+  coeffSubmit.addEventListener('click', coeffInput)
+}
+
+const coeffInput = (event) => {
+  event.preventDefault()
+
+  let coeffs = []
+
+  for (let i=0; i<degreeCopy; i++) {
+    coeffBox = document.getElementsByClassName(`${i}`)
+    coeffs[i] = coeffBox[0].value
+  }
+  console.log(coeffs)
 }
 
 const submitDegree = (event) => {
   event.preventDefault()
   appendInputs(degree.value)
+  degreeCopy = degree.value
 }
 
 submitBtn.addEventListener('click', submitDegree)
